8e0026132f1f6c95c72553643026177b6a4f81db
kapilan <166731523+kapilanramesh@users.noreply.github.com>
2025-04-08 17:46:03 +0530

    variable.tf

--- variable.tf
+++ variable.tf
@@ -1,21 +1,19 @@
 variable "aws_region" {
-  description = "AWS region"
+  description = "The AWS region to deploy resources in"
   type        = string
-  default     = "ap-south-1"
 }
 
 variable "s3_bucket_name" {
-  description = "Name of the S3 bucket to store Terraform state"
+  description = "The name of the S3 bucket to store the Terraform state"
   type        = string
 }
 
 variable "dynamodb_table_name" {
-  description = "Name of the DynamoDB table for Terraform state locking"
+  description = "The name of the DynamoDB table used for state locking"
   type        = string
 }
 
 variable "environment" {
-  description = "Environment name (e.g., dev, prod)"
+  description = "The deployment environment (e.g., dev, staging, prod)"
   type        = string
-  default     = "dev"
 }
