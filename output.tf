0fcfeb7d7f702b3ebe9e19ba834fbf37cabfc28b
kapilan <166731523+kapilanramesh@users.noreply.github.com>
2025-04-08 17:47:31 +0530

    output.tf

--- output.tf
+++ output.tf
@@ -1,7 +1,9 @@
 output "s3_bucket_name" {
-  value = aws_s3_bucket.terraform_state.id
+  description = "Name of the S3 bucket used for Terraform state"
+  value       = aws_s3_bucket.terraform_state.id
 }
 
 output "dynamodb_table_name" {
-  value = aws_dynamodb_table.terraform_locks.id
+  description = "Name of the DynamoDB table used for state locking"
+  value       = aws_dynamodb_table.terraform_locks.name
 }
